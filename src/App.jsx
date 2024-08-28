372c50ac0378098babe5e7e105f6ffcfbb4f2403
SravaniPanchireddy <sravanipanchireddy2004@gmail.com>
2024-08-28 10:14:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/sidebar'
+import Main from './Components/Main/main'
+
+const App=()=>{
+  return(
+    <>
+     <Sidebar/>
+     <Main/>
+    </>
+
+  )
+}
+export default App
+
